3fc426109b85be93919d6e552995e0ecccff3fef
Michael Ngo <michaelngo1337@yahoo.com>
2021-08-13 19:59:58 -0700

    changed colors and stylized about text

--- src/About.js
+++ src/About.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
 import Poodle from './Poodle.jpg'
 
 const useStyles = makeStyles((theme) => ({
@@ -10,8 +11,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     '& > *': {
       margin: theme.spacing(1),
-    },
-    color: 'white'
+    }
   },
   large: {
     width: theme.spacing(50),
@@ -20,6 +20,16 @@ const useStyles = makeStyles((theme) => ({
   },
   gridMargin: {
     marginTop: '6%'
+  },
+  gridText: {
+    marginLeft: '-4%'
+  },
+  paperBackground: {
+    backgroundColor: '#3E3D3D'
+  },
+  typo: {
+    color: 'white',
+    padding: '5%'
   }
 }));
 
@@ -32,11 +42,13 @@ const About = () => {
         <Grid item xs={6}>
           <Avatar alt="Poodle" src={Poodle} className={classes.large} />
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="h4" gutterBottom>
-            My name is Michael Ngo, aspiring full stack web developer. Here, you can see a variety of projects
-            that I have worked on.
-          </Typography>
+        <Grid item xs={6} className={classes.gridText}>
+          <Paper elevation={3} variant="outlined" className={classes.paperBackground}>
+            <Typography variant="h4" gutterBottom className={classes.typo}>
+              My name is Michael Ngo, aspiring full stack web developer. Here, you can see a variety of projects
+              that I have worked on.
+            </Typography>
+          </Paper>
         </Grid>
       </Grid>
     </div>
