10dcef3c40b837d14297d526cf84e5b29dcf4429
Michael Ngo <michaelngo1337@yahoo.com>
2021-08-13 22:57:57 -0700

    made card item for project section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import NavBar from './NavBar'
 import About from './About'
+import Projects from './Projects'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <About />
+      <Projects />
     </>
   )
 }
---
+++ src/Projects.js
@@ -0,0 +1,84 @@
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(() => ({
+  cards: {
+    maxWidth: 345,
+    backgroundColor: '#3E3D3D',
+    color: 'white'
+  },
+  projectTitle: {
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    marginTop: '18%'
+  },
+  buttonColor: {
+    color: '#80aaff'
+  }
+}))
+
+const Projects = () => {
+  const classes = useStyles()
+
+  return (
+    <div>
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <Typography variant="h2" gutterBottom className={classes.projectTitle}>
+            PROJECTS
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container spacing={0}>
+            <Grid item>
+              <Card className={classes.cards}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                      across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+                <CardActions>
+                  <Button size="small" className={classes.buttonColor}>
+                    Share
+                  </Button>
+                  <Button size="small" className={classes.buttonColor}>
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item>
+              
+            </Grid>
+            <Grid item>
+              
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
+
+export default Projects
